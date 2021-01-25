a76ddb6af7d9cb5d752963fb72b6b8874c43915e
Orcun Avsar <orc.avs@gmail.com>
2021-01-25 20:42:34 +0300

    More readable one liners.

--- house-robber/house_robber.py
+++ house-robber/house_robber.py
@@ -15,25 +15,16 @@ class Solution:
         # after i (including i)
         for i in range(len(nums)-1, -1, -1):
             # we will rob either i+2 or i+3 after i
-            a = i+2 < len(nums) and nums[i+2] or 0
-            b = i+3 < len(nums) and nums[i+3] or 0
-            # more open version of one liner for readability
-            # works slower :) 
-            # if i+2 < len(nums):
-            #    a = nums[i+2]
-            # else:
-            #    a = 0
-            # if i+3 < len(nums):
-            #    b = nums [i+3]
-            # else:
-            #    b = 0
+            a = nums[i+2] if (i+2 < len(nums)) else 0
+            b = nums[i+3] if (i+3 < len(nums)) else 0
 
             # pick one that will give us maximum
             nums[i] = max(a, b) + nums[i]
 
         # return element that will give maximum income
-        return nums and sorted(nums)[-1] or 0
+        return sorted(nums)[-1] if nums else 0
 
 
 if __name__ == '__main__':
+   print(Solution().rob([]))
    print(Solution().rob([2, 7, 9, 3, 1]))
