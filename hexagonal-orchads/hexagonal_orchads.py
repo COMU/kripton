8150d0ca436df380729a8551b341535f906c2987
Orcun Avsar <orc.avs@gmail.com>
2021-06-07 10:34:03 +0300

    Show complete percantage for big inputs.

--- hexagonal-orchads/hexagonal_orchads.py
+++ hexagonal-orchads/hexagonal_orchads.py
@@ -5,11 +5,11 @@ from sympy.ntheory.factor_ import totient
 
 def count_blinds_in_triangle(n):
     total = 0
-    counter = 1
     for level in range(2, ((n+1)//2)+1):
         t = totient(level)
         total += t * (int(n / level) - 1)
-        counter += 1
+        if level % 10000 == 0:
+            print("%{:.2f}".format((level/n)*100))
     return total
 
 
@@ -20,6 +20,7 @@ def main(n):
 
 
 if __name__ == '__main__':
-    n = 1000000
+    # takes about 2.5 hours to calculate 100 million
+    n = 100000000
     total_blinds = main(n)
     print('total blind points: %s' % total_blinds)
